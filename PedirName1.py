b1a059df9abd52309f17c01af71d95bb0f54c4c3
vetdy03 <cocaever0@gmail.com>
2025-03-23 17:35:47 -0400

    corrrigiendo el ericio 1

--- PedirName1.py
+++ PedirName1.py
@@ -3,5 +3,6 @@ def pedirName():
   age= int(input("{} How old are you?:  ".format(name)))
   print("Wellcome at the project {}, being {} is great ".format(name.upper(), age))
   print("See you soon")
+  
 if __name__ == '__main__':  
   pedirName()
