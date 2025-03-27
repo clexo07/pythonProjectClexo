f25cf46dd476eb13180d345fa3467afdf536f63c
ever coca <86172270+vetdy03@users.noreply.github.com>
2025-03-27 12:38:46 -0400

    ultima mejora del eje5

--- cadenas/ejerc5.py
+++ cadenas/ejerc5.py
@@ -1,6 +1,7 @@
 def ejercicio5GetNumber():
     invertedPhrase = input("Enter your phrase: ")
     print("Your inverted phrase is: {}".format(invertedPhrase[::-1]))
+    print("come back soon")
 
 if __name__ == "__main__":
     ejercicio5GetNumber()
