8d889304e0d04fdd8e51afaa21f5ccaacc5d09c2
cley xian <vetdy03@gmail.com>
2025-03-28 23:36:52 -0400

    Create ejer6BuscarVocal.py y mandar para su revision a vetdy03

---
+++ cadenas/ejer6BuscarVocal.py
@@ -0,0 +1,9 @@
+
+
+frase = input("Introduce una frase: ")
+vocal = input("Introduce una vocal en minúscula:  ")
+print(frase.replace(vocal, vocal.upper()))
+"""
+Ejercicio
+Escribir un programa que pida al usuario que introduzca una frase en la consola y una vocal en minúscula, y después muestre por pantalla la misma frase pero con la vocal introducida en mayúscula.
+"""
