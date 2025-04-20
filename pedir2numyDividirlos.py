d3a4c4532921c759e8de719284e23c60d5fa00b5
cley xian <vetdy03@gmail.com>
2025-04-20 02:25:34 -0400

    Update pedir2numyDividirlos.py

--- pedir2numyDividirlos.py
+++ pedir2numyDividirlos.py
@@ -1,4 +1,5 @@
-#Escribir un programa que pida al usuario dos números y devuelva su división. Si el usuario no introduce números debe devolver un aviso de error y si el divisor es cero también.
+#Escribir un programa que pida al usuario dos números y devuelva su división.
+#Si el usuario no introduce números debe devolver un aviso de error y si el divisor es cero también.
 n = float(input("Introduce el dividendo: "))
 m = float(input("Introduce el divisior: "))
 if m == 0:
